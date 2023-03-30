97676c7bb0e2302681ed15db17d33285c2a8da3e
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-03-30 10:59:16 -0300

    arreglos en grid

--- client/src/components/Grid/Grid.jsx
+++ client/src/components/Grid/Grid.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { getAllEventList } from "../../Redux/actions";
 import Card from "../Card/Card";
 // import styled, { keyframes } from "styled-components";
+import EventoNotFound from "../EventoNotFound/EvetoNotFound";
 import Loading from "../Loading/Loading";
 import { Carousel } from "react-responsive-carousel";
 import { Cube, NotFound } from "../Styles/Styles";
@@ -42,8 +43,8 @@ export default function Grid() {
   //-----------------------------------------------------
   let Events2 = []
 
-  for (let i = 0; i < Events.length; i += 5) {
-    let arr = Events.slice(i, i + 5);
+  for (let i = 0; i < Events.length; i += 4) {
+    let arr = Events.slice(i, i + 4);
     Events2?.push(arr);
   }
   // console.log("Arreglo: ", Events2);
@@ -51,16 +52,16 @@ export default function Grid() {
   //-----------------------------------------------------
   let Events18 = []
 
-  for (let i = 0; i < eventsFilter18.length; i += 5) {
-    let arr = eventsFilter18.slice(i, i + 5);
+  for (let i = 0; i < eventsFilter18.length; i += 4) {
+    let arr = eventsFilter18.slice(i, i + 4);
     Events18?.push(arr);
   }
   // console.log("Arreglo: ", Events18);
   //-----------------------------------------------------
   let Event17 = []
 
-  for (let i = 0; i < eventsFilter17.length; i += 5) {
-    let arr = eventsFilter17.slice(i, i + 5);
+  for (let i = 0; i < eventsFilter17.length; i += 4) {
+    let arr = eventsFilter17.slice(i, i + 4);
     Event17?.push(arr);
   }
   // console.log("Arreglo: ", Events2);
@@ -68,8 +69,8 @@ export default function Grid() {
   //-----------------------------------------------------
   let EventsFiltered = []
 
-  for (let i = 0; i < filtered.length; i += 5) {
-    let arr = filtered.slice(i, i + 5);
+  for (let i = 0; i < filtered.length; i += 4) {
+    let arr = filtered.slice(i, i + 4);
     EventsFiltered?.push(arr);
   }
   // console.log("Arreglo: ", Events2);
@@ -79,45 +80,55 @@ export default function Grid() {
   if(err.length >0){
 
     return (
-      <NotFound>EVENT NOT FOUND</NotFound>
+      <EventoNotFound/>
     )
   };
 
 
   if(filtered.length > 0){
+
+
     return (
       filtered.length ? (
-      <div>
-        <Carousel showThumbs={false}>
-        {
-          EventsFiltered.map( (eventos,i) => 
-            <Cube key={i} className="contenedor">
-              { 
-                eventos.map((event,i) => {
-                  return <Card event={event} key={i} />
-                })
-              }
-            </Cube>
-          )    
-        }
-      </Carousel>
-      </div>  
+
+        
+        <div className="tituloFiltrado">
+          <h1>Eventos filtrados</h1>
+
+          <div className="contGridFiltrado" >
+
+            { EventsFiltered.map((e, i) => 
+              
+              <div className="gridItem" key={i}>
+
+                { 
+                  e.map((event,i) => {
+                    return <Card event={event} key={i} />
+                  })
+                }     
+              </div>
+
+            )}
+
+          </div>
+        </div>
       
+
       ): <Loading/>
-    ) 
-  };
+      ) 
+    };
   //----------------------------------------------------
   return(
 
     Events.length ? (
-    <div>
-      <p>All events</p>
+    <div className="contGrid01">
+      <p className="tagP">Todos los eventos</p>
 
       <Carousel showThumbs={false}>
         {
           Events2.map( (eventos,i) => 
             
-            <Cube key={i} className="contenedor">
+          <Cube key={i} className="contenedor">
       
               { 
                 eventos.map((event,i) => {
@@ -127,18 +138,18 @@ export default function Grid() {
             
             </Cube>
           )
-        
+          
         }
       </Carousel>
       
         <div>
 
-          <p>events +18</p>
+          <p className="tagP">Eventos +18</p>
 
           <Carousel slidesToShow={1}>
             {
               Events18.map( (eventos,i) => 
-                
+              
                 <Cube key={i} className="contenedor">
           
                   { 
@@ -149,20 +160,20 @@ export default function Grid() {
                 
                 </Cube>
               )
-            
+              
             }
           </Carousel>
         </div>
       
       
 
-      <p>events -18</p>
+      <p className="tagP">Eventos -18</p>
 
       <Carousel slidesToShow={1}>
         {
           Event17.map( (eventos,i) => 
-            
-            <Cube key={i} className="contenedor">
+          
+          <Cube key={i} className="contenedor">
       
               { 
                 eventos.map((event,i) => {
@@ -182,7 +193,7 @@ export default function Grid() {
           let eventFil = Events.filter( e => e.typeEvent?.genre === genero);
           // console.log( "eventmap",eventFil);
           let EventsGenre = []
-
+          
           for (let i = 0; i < eventFil.length; i += 5) {
             let arr = eventFil.slice(i, i + 5);
             EventsGenre?.push(arr);
@@ -192,13 +203,13 @@ export default function Grid() {
 
           return(<div>
 
-            <p>{eventFil[0]?.typeEvent? eventFil[0]?.typeEvent.type : "Event Created"} - {eventFil[0]?.typeEvent? eventFil[0]?.typeEvent.genre : null}</p>
+            <p className="tagP">{eventFil[0]?.typeEvent? eventFil[0]?.typeEvent.type : "Event Created"} - {eventFil[0]?.typeEvent? eventFil[0]?.typeEvent.genre : null}</p>
 
             <Carousel slidesToShow={1}>
               {
                 EventsGenre.map( (eventos,i) => 
-                  
-                  <Cube key={i} className="contenedor">
+                
+                <Cube key={i} className="contenedor">
             
                     { 
                       eventos.map((event,i) => {
@@ -208,25 +219,44 @@ export default function Grid() {
                   
                   </Cube>
                 )
-              
+                
               }
             </Carousel> 
 
           </div>)
           
         })
-
+        
       }
        
     </div>
     ): <Loading/>
-  )
+    )
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+  
+  /*<div className="contGrid01">
 
+    <p className="tagP">Eventos filtrados</p>
 
-
-
-
-
-
-
-}
+    <Carousel showThumbs={false}>
+    {
+      EventsFiltered.map( (eventos,i) => 
+        <Cube key={i} className="contenedor">
+          { 
+            eventos.map((event,i) => {
+              return <Card event={event} key={i} />
+            })
+          }
+        </Cube>
+      )    
+    }
+  </Carousel>
+  </div>*/ 
