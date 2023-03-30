760ad399ee9cab3d212302790ae58a5b71506551
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-03-30 10:58:16 -0300

    usuario  baneado

--- client/src/components/UserBanned/UserBanned.jsx
+++ client/src/components/UserBanned/UserBanned.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
+import "./UserBanned.css"
 
 export default function UserBanned (){
 
@@ -9,13 +10,14 @@ export default function UserBanned (){
 
         <div>
             <Navbar />
+            
+            <div className="ban">
 
-            <h1>Eventix</h1>
-
-            <h3>Esta cuenta ha sido baneada 😧 😢</h3>
-            <p>Contactar con administracion para mas detlles. 😎</p>
-            <p> 📧 eventix@hotmail.com 🚀</p>
-            {/*<Link to='eventix@gamil.com' onClick={(e) => { window.location.href = "eventix@gamil.com"; e.preventDefault();}}> eventix@gamil.com</Link>*/}
+                <h2>Esta cuenta ha sido baneada 😧 😢</h2>
+                <p>Contactar con administracion para mas detlles. 😎</p>
+                <p> 📧 eventix@hotmail.com 🚀</p>
+                {/*<Link to='eventix@gamil.com' onClick={(e) => { window.location.href = "eventix@gamil.com"; e.preventDefault();}}> eventix@gamil.com</Link>*/}
+            </div>
 
         </div>
     )
