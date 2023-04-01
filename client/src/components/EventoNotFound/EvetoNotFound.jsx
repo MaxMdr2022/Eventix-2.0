0b0e6a477d7a2424b82f3cf3e817d0a5ec96d23f
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-04-01 13:17:00 -0300

    not events

---
+++ client/src/components/EventoNotFound/EvetoNotFound.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function EventoNotFound (){
+
+
+    return (
+
+        <div className="tituloFiltrado">
+          <h1>Eventos filtrados</h1>
+
+          <div className="contGridFiltrado" >
+
+            <h1 className="noEvento">No se encontró evento</h1>
+
+          </div>
+        </div>
+    )
+};
