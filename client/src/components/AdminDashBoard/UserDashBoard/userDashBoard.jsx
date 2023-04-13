b50b9e1f763bde25c7205761976c8076a8f08359
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-04-13 14:19:48 -0300

    alert

--- client/src/components/AdminDashBoard/UserDashBoard/userDashBoard.jsx
+++ client/src/components/AdminDashBoard/UserDashBoard/userDashBoard.jsx
@@ -54,6 +54,7 @@ export default function UserDashBoard (){
         };
 
         dispatch(userUpdate(id, usuarioModificado));
+        window.location.reload();
         alert("Perfil modificado con exito.");
     
     };
@@ -63,8 +64,8 @@ export default function UserDashBoard (){
         e.preventDefault();
 
         dispatch(userUpdate(id, {isAdmin: true}));
-        window.location.reload();
-        // alert("Usuario con Permisos de Administrador");
+        
+        alert("Usuario con Permisos de Administrador");
     };
 
     const handleRemoveAdmin= (e)=> {
@@ -74,7 +75,7 @@ export default function UserDashBoard (){
         dispatch(userUpdate(id, {isAdmin: false}));
    
         window.location.href=window.location.href;
-        // alert("Usuario sin Permisos de Administrador");
+        alert("Usuario sin Permisos de Administrador");
     };
 
     const handleIsBanned = (e)=> {
@@ -83,7 +84,7 @@ export default function UserDashBoard (){
 
         dispatch(userUpdate(id, {isBanned: true}));
         window.location.reload();
-        // alert("Usuario Baneado");
+        alert("Usuario Baneado");
     };
 
     const handleRemoveBanned = (e)=> {
@@ -93,7 +94,7 @@ export default function UserDashBoard (){
         dispatch(userUpdate(id, {isBanned: false}));
    
         history.go(0)
-        // alert("Usuario desbaneado");
+        alert("Usuario desbaneado");
     };
 
     const cancel = (e) => {
