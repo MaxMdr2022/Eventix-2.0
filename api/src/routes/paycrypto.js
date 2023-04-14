fcad6823bf4c27c3c7a7a0537d78dda334e9779b
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-04-14 19:46:32 -0300

    arreglo de strings

--- api/src/routes/paycrypto.js
+++ api/src/routes/paycrypto.js
@@ -58,7 +58,7 @@ route.post("/create-charge", async(req,res)=>{   // ruta de pago http://localhos
         metadata: {                        // info del comprador. para guardar en la base de datos.
             customer_id: id_user,
             // customer_name: "Maxi Meder",
-            customer_id_ticket: ticketId  //guardo el id de cada ticket
+            customer_id_ticket: [ticketId]  //guardo el id de cada ticket
         },
         redirect_url: `${DOMAIN}`, // ${DOMAIN}/perfilusuario/pago   cuando el pago se finaliza le sale un boton para continuar. Esa url es donde lo va a redirigir el boton. NOTA: tiene que ser un dominio https. Si no coinbase no redirecciona. 
         cancel_url: `${DOMAIN}`,  // cuando se calcele el pago, va a redireccionar a esta ruta.
