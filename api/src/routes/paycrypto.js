c5063596eebd3879fa74e543602adf6cd199396e
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-04-14 17:53:18 -0300

    perfil

--- api/src/routes/paycrypto.js
+++ api/src/routes/paycrypto.js
@@ -60,8 +60,8 @@ route.post("/create-charge", async(req,res)=>{   // ruta de pago http://localhos
             // customer_name: "Maxi Meder",
             customer_id_ticket: ticketId  //guardo el id de cada ticket
         },
-        redirect_url: `${DOMAIN}/perfil`, // ${DOMAIN}/perfilusuario/pago   cuando el pago se finaliza le sale un boton para continuar. Esa url es donde lo va a redirigir el boton. NOTA: tiene que ser un dominio https. Si no coinbase no redirecciona. 
-        cancel_url: `${DOMAIN}/perfil`,  // cuando se calcele el pago, va a redireccionar a esta ruta.
+        redirect_url: `${DOMAIN}`, // ${DOMAIN}/perfilusuario/pago   cuando el pago se finaliza le sale un boton para continuar. Esa url es donde lo va a redirigir el boton. NOTA: tiene que ser un dominio https. Si no coinbase no redirecciona. 
+        cancel_url: `${DOMAIN}`,  // cuando se calcele el pago, va a redireccionar a esta ruta.
     };
 
     const charge = await Charge.create(chargeData);  // le pasamos los datos para que cree la orden de pago. charge es un json con toda la info. donde vamos a tomar el parametro hosted_url que contiene la url que nos manda a la pasarela de pago
